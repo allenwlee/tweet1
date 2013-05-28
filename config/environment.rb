5f5cfc79e513d7b9b92a4dfe0340a2332e479898
Apprentice <apprentice@devbootcamp.com>
2013-05-28 10:34:47 -0700

    second and last

--- config/environment.rb
+++ config/environment.rb
@@ -19,6 +19,7 @@ require 'sinatra'
 require "sinatra/reloader" if development?
 
 require 'erb'
+require 'twitter'
 
 # Some helper constants for path-centric logic
 APP_ROOT = Pathname.new(File.expand_path('../../', __FILE__))
@@ -31,3 +32,10 @@ Dir[APP_ROOT.join('app', 'helpers', '*.rb')].each { |file| require file }
 
 # Set up the database and models
 require APP_ROOT.join('config', 'database')
+
+Twitter.configure do |config|
+  config.consumer_key = 'XtepiWcGFh9reYudRP69wA'
+  config.consumer_secret = 'uWv9Kow4SUCVL9zg0pxNzOyUoXwdmy4FreYZ4IuxVg'
+  config.oauth_token = '17456158-YeHPfBaXJA8r8d1xUv58wOMTyg5jnMuHpupGqQuDU'
+  config.oauth_token_secret = 'ywfjBlwLEyYLzEkzO820r4EUZIZwMxb20T9d3woN8'
+end
